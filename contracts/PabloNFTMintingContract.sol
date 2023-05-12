9f3e91dcc181cdcdc01e74fbbb001018fd65af7d
Lukas Ehlers <ehlers@outson.com>
2023-05-12 10:25:12 +0200

    adding NFTContract

---
+++ contracts/PabloNFTMintingContract.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MyToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    using SafeERC20 for IERC20;
+
+    Counters.Counter private _tokenIdCounter;
+
+    mapping(address => bool) public whitelist;
+    mapping(address => bool) public minted;
+
+    IERC20 public immutable paymentToken;
+    uint256 public immutable fee;
+
+    event AddedToWhitelist(address[] addresses);
+    event RemovedFromWhitelist(address[] addresses);
+    
+    constructor(address _paymentToken, uint256 _fee) ERC721("MyToken", "MTK") {
+        paymentToken = IERC20(_paymentToken);
+        fee = _fee;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://bafybeigwu25an0/";
+    }
+
+    function safeMint(address to) public {
+        require(whitelist[msg.sender], "Address not in whitelist");
+        require(!minted[msg.sender], "Already minted");
+        paymentToken.safeTransferFrom(msg.sender, address(this), fee);
+        minted[msg.sender] = true;
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+
+        _safeMint(to, tokenId);
+    }
+
+    function addToWhitelist(address[] calldata toAddAddresses) 
+    external onlyOwner
+    {
+        for (uint i = 0; i < toAddAddresses.length; i++) {
+            whitelist[toAddAddresses[i]] = true;
+        }
+        emit AddedToWhitelist(toAddAddresses);
+    }
+
+    /**
+     * @notice Remove from whitelist
+     */
+    function removeFromWhitelist(address[] calldata toRemoveAddresses)
+    external onlyOwner
+    {
+        for (uint i = 0; i < toRemoveAddresses.length; i++) {
+            delete whitelist[toRemoveAddresses[i]];
+        }
+        emit RemovedFromWhitelist(toRemoveAddresses);
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = paymentToken.balanceOf(address(this));
+        paymentToken.safeTransfer(owner(), balance);
+    }
+}
